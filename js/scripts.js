693e7c5d59aa9fa6892ab4f1554bedd015595ae6
Krzysztof Stankowski <Krzysztof Stankowski>
2017-01-31 18:30:37 +0100

    new js exercise

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+var text = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.';
+
+var animal = 'Zielone słonie';
+
+var animal = animal.toUpperCase();
+console.log(animal);
+
+var text = text.replace('Papugi', animal);
+console.log(text);
+
+console.log(text.length/2);
+
+var text = text.substr(0, 45)
+
+console.log(text);
